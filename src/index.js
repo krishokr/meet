a23eb7a8fd61699dcbc3ffa7ed0b14f4fdf813fe
Kris Hokr <khokr@ucdavis.edu>
2022-03-10 09:30:14 -0800

    moved all files from meet folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import App from './meet/src/App';
+import * as serviceWorkerRegistration from '../serviceWorkerRegistration';
+import reportWebVitals from '../reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
