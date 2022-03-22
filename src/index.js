21cef816284c02db388e0e740d503faa6a0f85fc
Kris Hokr <khokr@ucdavis.edu>
2022-03-22 11:19:09 -0700

    added atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './Styles/index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('d275a365a51b4155abf3d8de3966a6ec').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +18,5 @@ ReactDOM.render(
 serviceWorkerRegistration.unregister();
 
 reportWebVitals();
+
+
