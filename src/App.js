9557b43eafcfb2120aa31ca39df71e317c02082d
Kris Hokr <khokr@ucdavis.edu>
2022-04-08 10:27:57 -0700

    added splash page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './Styles/nprogress.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { extractLocations, getEvents } from './api';
+import { getEvents, extractLocations, checkToken, getAccessToken } from
+'./api';
 import logo from './logo.png';
 import { OfflineAlert } from './Alert';
+import WelcomeScreen from './WelcomeScreen';
+
 
 export default class App extends Component {
   state = {
@@ -14,17 +17,35 @@ export default class App extends Component {
     events: [],
     locations: [],
     currentLocation: '',
-    numberOfEvents: 5
+    numberOfEvents: 5,
+    showWelcomeScreen: undefined
   }
 
-  componentDidMount() {
-    this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({allEvents: events, locations: extractLocations(events)});
-        this.setState({events: events.slice(0,5)});
-      }
-    })
+  // componentDidMount() {
+  //   this.mounted = true;
+  //   getEvents().then((events) => {
+  //     if (this.mounted) {
+  //       this.setState({allEvents: events, locations: extractLocations(events)});
+  //       this.setState({events: events.slice(0,5)});
+  //     }
+  //   })
+  // }
+
+  async componentDidMount() {
+      this.mounted = true;
+      const accessToken = localStorage.getItem('access_token');
+      const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
+      const searchParams = new URLSearchParams(window.location.search);
+      const code = searchParams.get("code");
+      this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+      if ((code || isTokenValid) && this.mounted) {
+        getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ allEvents: events, locations: extractLocations(events), events: events.slice(0,5) });
+        }
+      });
+    }
   }
 
   componentWillUnmount() {
@@ -72,6 +93,9 @@ export default class App extends Component {
 
   render() {
 
+    if (this.state.showWelcomeScreen === undefined) return <div
+      className="App" />
+      
     return (
       <div className="App">
         
@@ -83,6 +107,7 @@ export default class App extends Component {
         </div>
         
         <EventList events={this.state.events}/>
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}getAccessToken={() => { getAccessToken() }} />
       </div>
     );
 
--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -1,38 +1,23 @@
+import './Styles/WelcomeScreen.css';
+import logo from './logo.png';
+
 function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
-    (
-    <div className="WelcomeScreen">
-    <h1>Welcome to the Meet app</h1>
-    <h4>
-    Log in to see upcoming events around the world for
-    full-stack
-    developers
-    </h4>
-    <div className="button_cont" align="center">
-    <div class="google-btn">
-    <div class="google-icon-wrapper">
-    <img
-    class="google-icon"
-    src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
-    o.svg"
-    alt="Google sign-in"
-    />
-    </div>
-    <button onClick={() => { props.getAccessToken() }}
-    rel="nofollow noopener"
-    class="btn-text">
-    <b>Sign in with google</b>
-    </button></div>
-</div>
-<a
-href="https://YOUR_GITHUB_USERNAME.github.io/meet/privacy.html"
-rel="nofollow noopener"
->
-Privacy policy
-</a>
-</div>
-)
-: null
+    (<div className="WelcomeScreen">
+        <img className='welcome-logo' alt='logo' src={logo} />
+        <div className="button_cont" align="center">
+            <div class="google-btn">
+                <div class="google-icon-wrapper">
+                    <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google sign-in"/>
+                    
+                 </div>
+                <button onClick={() => { props.getAccessToken() }} rel="nofollow noopener" class="btn-text"><b>Sign in with google</b></button>
+            </div>
+        </div>
+        <a href="https://krishokr.github.io/meet/privacy.html" rel="nofollow noopener">Privacy policy</a>
+    </div>) : null
 }
+
+
 export default WelcomeScreen;
 
